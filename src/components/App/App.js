6e99d5a5d583abd4345ba4e86e98d56398a11c32
maberer1 <madison.aberer@gmail.com>
2022-08-05 15:28:08 -0700

    Preliminary React Routes set up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,35 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router';
+//import SignIn
+//import SignUp
+//import BrowsePage
+//import ResultsPage
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      test
+      <Switch>
+
+        <Route exact path="/">
+          <SignIn />
+        </Route>
+
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+
+        <Route path="/search">
+          <BrowsePage />
+        </Route>
+
+        <Route path="/searchresults">
+          <ResultsPage />
+        </Route>
+
+      </Switch>
     </div>
+    </Router>
   );
 }
 
