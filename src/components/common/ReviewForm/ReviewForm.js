4cdab7c5db30b7c126d3f3f2b5e794fea6d58468
maberer1 <madison.aberer@gmail.com>
2022-08-09 16:17:10 -0700

    Write ReviewForm component

--- src/components/common/ReviewForm/ReviewForm.js
+++ src/components/common/ReviewForm/ReviewForm.js
@@ -1,67 +1,176 @@
-import { toHaveDescription } from '@testing-library/jest-dom/dist/matchers';
-import React from 'react';
-import StarRating from './StarRating';
-
+import React from "react";
 class ReviewForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {
+          title: "Title",
+          comment: "Your comment",
+          noiseLevel: null,
+          outlets: null,
+          wifi: null,
+          aesthetic: null,
+        };
+    
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+      handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+      }
 
-        // this.handleChange = this.handleChange(bind)(this);
-        // this.handleSubmit = this.handleSubmit(bind)(this);
+      handleClick(event) {
+        event.preventDefault()
+
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+          [name]: value
+        });
+      }
+
+      handleSubmit(event) {
+        event.preventDefault()
+        console.log(this.state.title)
+        console.log(this.state.comment)
+        console.log(this.state.noiseLevel)
+        console.log(this.state.outlets)
+        console.log(this.state.wifi)
+        console.log(this.state.aesthetics)
+      }
+    
+      render() {
+        return (
+          <form>
+            <label>
+              Title:
+              <input
+                name="title"
+                type="text"
+                value={this.state.title}
+                onChange={this.handleInputChange} />
+            </label>
+            <br/>
+            <label>
+              Comment:
+              <input
+                name="comment"
+                type="text"
+                value={this.state.comment}
+                onChange={this.handleInputChange} />
+            </label>
+            <br/>
+            <label>
+              Noise Level:
+              <button
+                name="noiseLevel"
+                value={1}
+                onClick={this.handleClick}/>
+              <button
+                name="noiseLevel"
+                value={2}
+                onClick={this.handleClick}/>
+              <button
+                name="noiseLevel"
+                value={3}
+                onClick={this.handleClick}/>
+              <button
+                name="noiseLevel"
+                value={4}
+                onClick={this.handleClick}/>
+              <button
+                name="noiseLevel"
+                value={5}
+                onClick={this.handleClick}/>
+            </label>
+            <br/>
+            <label>
+              Outlets:
+              <button
+                name="outlets"
+                value={1}
+                onClick={this.handleClick}/>
+              <button
+                name="outlets"
+                value={2}
+                onClick={this.handleClick}/>
+              <button
+                name="outlets"
+                value={3}
+                onClick={this.handleClick}/>
+              <button
+                name="outlets"
+                value={4}
+                onClick={this.handleClick}/>
+              <button
+                name="outlets"
+                value={5}
+                onClick={this.handleClick}/>
+            </label>
+            <br/>
+            <label>
+              Wifi:
+              <button
+                name="wifi"
+                value={1}
+                onClick={this.handleClick}/>
+              <button
+                name="wifi"
+                value={2}
+                onClick={this.handleClick}/>
+              <button
+                name="wifi"
+                value={3}
+                onClick={this.handleClick}/>
+              <button
+                name="wifi"
+                value={4}
+                onClick={this.handleClick}/>
+              <button
+                name="wifi"
+                value={5}
+                onClick={this.handleClick}/>
+            </label>
+            <br/>
+            <label>
+              Aesthetics:
+              <button
+                name="aesthetics"
+                value={1}
+                onClick={this.handleClick}/>
+              <button
+                name="aesthetics"
+                value={2}
+                onClick={this.handleClick}/>
+              <button
+                name="aesthetics"
+                value={3}
+                onClick={this.handleClick}/>
+              <button
+                name="aesthetics"
+                value={4}
+                onClick={this.handleClick}/>
+              <button
+                name="aesthetics"
+                value={5}
+                onClick={this.handleClick}/>
+            </label>
+            <br/>
+            <button
+              type="submit"
+              onClick={this.handleSubmit}>Post</button>
+          </form>
+        );
+      }
     }
-
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-    }
-
-render(){
-    return (
-        <div>
-            <form onSubmit={this.handleSubmit}>
-
-                <label>
-                    Title:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-
-                <label>
-                    Comment:
-                    <input type="textarea" value={this.state.value} onChange={this.handleChange} />
-                </label>
-
-                <label>
-                    Noise Level:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                    <StarRating/>
-                </label>
-
-                <label>
-                    Outlets:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                    <StarRating/>
-                </label>
-
-                <label>
-                    Wifi:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                    <StarRating/>
-                </label>
-
-                <label>
-                    Aesthetic:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                    <StarRating/>
-                </label>
-
-            </form>
-        </div>
-    );
-}
-}
 
 export default ReviewForm;
