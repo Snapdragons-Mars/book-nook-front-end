474d088bb6927d5ddc3b644d68805b1eb83f56b9
lancechincodes <lanceechin@gmail.com>
2022-08-09 19:11:53 -0500

    PIVOT

--- src/components/common/Search.js
+++ src/components/common/Search.js
@@ -1,32 +1,38 @@
-import React, { useState } from "react";
-import { Router } from "react-router";
-function SearchFunction() {
-  const [userCity, setUserCity] = useState("");
-  const handleChange = (e) => {
-    e.preventDefault();
-    setUserCity(e.target.value);
+// import React, { useState } from "react";
+// import { Router } from "react-router";
 
-    const sdk = require("api")("@fsq-docs/v1.0#3qzzviakl47e7mgp");
+// function Search() {
+//   const [userCity, setUserCity] = useState("");
+//   const handleChange = (e) => {
+//     e.preventDefault();
+//     setUserCity(e.target.value);
 
-    sdk.auth("null");
-    sdk
-      .placeSearch({
-        query: userCity,
-        categories: "coffee%2C%20boba%2C%20library%2C%20park%2C%20book%20store",
-        open_now: "true",
-      })
-      .then((res) => console.log(res))
-      .catch((err) => console.error(err));
-  };
-  return (
-    <div>
-      <input
-        type="search"
-        placeholder="Please Enter Your City"
-        onChange={handleChange}
-        value={userCity}
-      />
-    </div>
-  );
-}
-export default SearchFunction;
+//   //   const sdk = require("api")("@fsq-docs/v1.0#3qzzviakl47e7mgp");
+
+//   //   sdk.auth("null");
+//   //   sdk
+//   //     .placeSearch({
+//   //       query: userCity,
+//   //       categories: "coffee%2C%20boba%2C%20library%2C%20park%2C%20book%20store",
+//   //       open_now: "true",
+//   //     })
+//   //     .then((res) => console.log(res))
+//   //     .catch((err) => console.error(err));
+//   // };
+
+
+
+//   return (
+//     // <div>
+//       <input
+//         type="search"
+//         placeholder="Please Enter Your City"
+//         onChange={handleChange}
+//         value={userCity}
+//       />
+//     </div>
+//   );
+// }
+
+// }
+// export default Search;
